0c9e740bfcafd4acea7dc79e69520a50b0b2b864
Dulat <dulat.kussainov@gmail.com>
2022-11-02 12:53:36 -0400

    commit tree b1679458dffa46b4dfac344d5051e78711c90209

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,22 @@
-# your code goes here
+require 'pry'
+
+class Person
+    attr_reader :name
+    attr_accessor :bank_account, :happiness, :hygiene
+    def initialize(name, bnkacc =25, happiness = 8, hygiene = 8)
+        @name = name
+        @bank_account = bnkacc
+        @happiness = happiness
+        @hygiene = hygiene
+
+    end
+    def happiness(num)
+        @happiness = if num > 10
+            10
+        elsif num < 0
+            0
+        end
+    end
+    binding.pry
+
+end
